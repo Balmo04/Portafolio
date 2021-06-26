71f6bd85a849c72e26d2e4e2d292ad73c9248d27
Emerson04-sv <balmore04ebao@gmail.com>
2021-06-25 19:36:03 -0600

    arregle el punto extension del codigo

--- js/objects.js
+++ js/objects.js
@@ -8,7 +8,7 @@ class proyect {
     }
 }
 
-const creditCard= new proyect (1, "img/credit-value.jpg", "Credit Value", "https://Tarjeta-de-validaci-n-4.emerson04sv.repl.co", "https://github.com/Emerson04-sv/Tarjeta-de-validaci-n.git");
-const caloriaApp= new proyect (2, "img/caloria-app.jpg", "Caloria App", "https://CaloriasApp-2.emerson04sv.repl.co", "https://github.com/Emerson04-sv/CaloriasApp.git");
-const triviaApp= new proyect (3, "img/trivia-app.jpg", "Trivia App", "https://Trivia-App-1.emerson04sv.repl.co", "https://github.com/Emerson04-sv/Trivia-App.git");
+const creditCard= new proyect (1, "img/credit-value.JPG", "Credit Value", "https://Tarjeta-de-validaci-n-4.emerson04sv.repl.co", "https://github.com/Emerson04-sv/Tarjeta-de-validaci-n.git");
+const caloriaApp= new proyect (2, "img/caloria-app.JPG", "Caloria App", "https://CaloriasApp-2.emerson04sv.repl.co", "https://github.com/Emerson04-sv/CaloriasApp.git");
+const triviaApp= new proyect (3, "img/trivia-app.JPG", "Trivia App", "https://Trivia-App-1.emerson04sv.repl.co", "https://github.com/Emerson04-sv/Trivia-App.git");
 const proyects=[creditCard, caloriaApp, triviaApp];
